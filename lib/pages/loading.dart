44cffc7423203594bb25fb274789829e161294bc
adi.boyii <adireyes00@gmail.com>
2022-04-12 15:44:19 +0800

    passing data

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -18,11 +18,17 @@ class _LoadingState extends State<Loading> {
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
 
     await instance.getTime();
-    print(instance.time);
-
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
     });
+
+    // print(instance.time);
+
+    // setState(() {
+    //   time = instance.time;
+    // });
   }
 
   @override
@@ -34,10 +40,8 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50),
-        child: Text(time),
-      ),
-    );
+        body: Center(
+      child: Text('Loading data'),
+    ));
   }
 }
