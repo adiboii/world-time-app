0ecba98b152f76cdfe90fdebde77a7fffd58687b
adi.boyii <adireyes00@gmail.com>
2022-04-12 15:29:04 +0800

    started with world time implementation

---
+++ lib/pages/loading.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:world_time_app/services/world_time.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+
+    await instance.getTime();
+    print(instance.time);
+
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text(time),
+      ),
+    );
+  }
+}
---
+++ lib/services/world_time.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String location; //location name
+  String time = ''; //the time in the location
+  String flag; //url asset for flag icon
+  String url; //location url for api endpoints
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    String uri = "http://worldtimeapi.org/api/timezone/$url";
+    Response response = await get(Uri.parse(uri));
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    //get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    // print(datetime);
+    // print(offset);
+
+    //create datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    time = now.toString();
+  }
+}
